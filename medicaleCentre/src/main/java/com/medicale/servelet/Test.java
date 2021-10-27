424b05d0302f237c46db3a089d2c6b002a0102f4
faridaCH <farida.chelhi06@gmail.com>
2021-10-27 16:23:29 +0200

    le commit de medicalCentre  premiere Servelet

---
+++ medicaleCentre/src/main/java/com/medicale/servelet/Test.java
@@ -0,0 +1,79 @@
+package com.medicale.servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test
+ */
+@WebServlet("/bonjour")
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Test() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println(" je suis dans le doPost");
+
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String dateNaissance = request.getParameter("dateNaissance");
+		String adresse = request.getParameter("adresse");
+		String pays = request.getParameter("pays");
+		String ville = request.getParameter("ville");
+		if (nom == null)
+			nom = "";
+		if (prenom == null)
+			prenom = "";
+		if (dateNaissance == null)
+			dateNaissance = " abscence de date de naissance ";
+
+		response.setContentType("text/html");
+		try (PrintWriter out = response.getWriter()) {
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("    <head>");
+			out.println("        <title>Page prenom</title>");
+			out.println("    </head>");
+			out.println("    <body>");
+			out.println("        <h1>Bonjour " + nom + " " + prenom + "</h1>");
+//			
+
+			out.println("<p>   vous etes né le  :  " + dateNaissance + "</p>");
+			out.println(" <p> adresse complet : " + adresse + " " + ville + " " + pays + "</p>");
+			out.println("    </body>");
+			out.println("</html>");
+
+		}
+	}
+
+}
